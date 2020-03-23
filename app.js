dea89a9d260017544f3a6b702e24612709bbe6de
Aneta Szajowska <an.szajowska@gmail.com>
2020-03-23 21:46:27 +0100

    first version

---
+++ app.js
@@ -0,0 +1,19 @@
+const hamburgerBtn = document.querySelector(".hamburger-button");
+const navBar = document.querySelector('.main-nav');
+
+hamburgerBtn.addEventListener('click', () => {
+    navBar.classList.toggle('visible');
+//     if (navBar.classList.contains("visible")) {
+//         navBar.classList.remove('visible');
+//    } else {
+//         navBar.classList.add("visible");
+       
+//    }
+});
+
+
+
+
+
+
+
